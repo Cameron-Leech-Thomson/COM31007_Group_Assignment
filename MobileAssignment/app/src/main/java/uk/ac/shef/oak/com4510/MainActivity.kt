877b4677048db61bb26c15f8b8d6d7788fcc047e
Mircea-Gelu Egry <mirceaegry72@gmail.com>
2021-12-15 12:52:45 +0000

    design and functionality update

--- MobileAssignment/app/src/main/java/uk/ac/shef/oak/com4510/MainActivity.kt
+++ MobileAssignment/app/src/main/java/uk/ac/shef/oak/com4510/MainActivity.kt
@@ -1,7 +1,25 @@
 package uk.ac.shef.oak.com4510
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar!!.hide()
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        var appBarConfiguration = AppBarConfiguration.Builder(R.id.navigation_home,
+            R.id.navigation_paths, R.id.navigation_gallery, R.id.navigation_user).build()
+
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(navView, navController)
+    }
 }
