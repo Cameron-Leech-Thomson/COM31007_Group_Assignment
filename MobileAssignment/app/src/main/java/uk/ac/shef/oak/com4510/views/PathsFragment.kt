810d94c5dd2bfbebf545c40d30ce594abfb54586
Ibrahim Teksoylu <iteksoylu1@sheffield.ac.uk>
2021-12-16 17:21:57 +0000

    Paths Page made

---
+++ MobileAssignment/app/src/main/java/uk/ac/shef/oak/com4510/views/PathsFragment.kt
@@ -0,0 +1,22 @@
+package uk.ac.shef.oak.com4510.views
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import uk.ac.shef.oak.com4510.R
+
+class PathsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        var v: View = inflater.inflate(R.layout.fragment_path, container, false)
+
+
+        return v
+    }
+}
