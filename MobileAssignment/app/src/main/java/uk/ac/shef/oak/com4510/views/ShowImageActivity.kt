9a02eede32b69d55901d3d238e51abde1af46212
Cameron Leech-Thomson <cameronleechthomson@gmail.com>
2021-12-17 00:44:25 +0000

    Can open an image specifically
    However not full size - so needs work

--- MobileAssignment/app/src/main/java/uk/ac/shef/oak/com4510/views/ShowImageActivity.kt
+++ MobileAssignment/app/src/main/java/uk/ac/shef/oak/com4510/views/ShowImageActivity.kt
@@ -1,12 +1,18 @@
 package uk.ac.shef.oak.com4510.views
 
 import android.app.Activity
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import uk.ac.shef.oak.com4510.R
 import uk.ac.shef.oak.com4510.model.Image
 import uk.ac.shef.oak.com4510.model.Path
 import uk.ac.shef.oak.com4510.viewmodels.ImageRepository
@@ -22,8 +28,12 @@ class ShowImageActivity : Activity(), Serializable {
         super.onCreate(savedInstanceState)
         imageID = intent.getSerializableExtra("image id") as Int
 
+        setContentView(R.layout.activity_image)
+
         val imageRepo = ImageRepository(this.application)
         val pathRepository = PathRepository(this.application)
+
+        val imageView: ImageView = findViewById(R.id.image_full)
 
         lateinit var selectedImage: Image
         lateinit var imagePath: Path
@@ -39,12 +49,20 @@ class ShowImageActivity : Activity(), Serializable {
         }
 
         // Get image data:
+        val uri = selectedImage.imageUri
         val longitude = selectedImage.longitude
         val latitude = selectedImage.latitude
         val date = selectedImage.time
         val temp = selectedImage.temperature
         val press = selectedImage.pressure
         val humid = selectedImage.humidity
+
+        imageView.setImageURI(Uri.parse(uri))
+
+        val backButton = findViewById<FloatingActionButton?>(R.id.fab_back)
+        backButton.setOnClickListener {
+            Log.d("ShowImage", "BackButton")
+        }
     }
 
 }
